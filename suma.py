e447bee13464dd26754d4ea3a8c759261f466c8d
Mauricio Sánchez <sanchezaguilarmauricio56@gmail.com>
2025-11-10 11:02:42 -0500

    sum.py changed

--- suma.py
+++ suma.py
@@ -1,7 +1,7 @@
 def main() -> None:
     try:
         numero_1 = float(input("Ingresa el primer número: "))
-        numero_2 = float(input("Ingresa el segundo número: "))
+        numero_2 = float(input("Ingresa el segundo número "))
     except ValueError:
         print("Entrada inválida. Asegúrate de ingresar números.")
         return

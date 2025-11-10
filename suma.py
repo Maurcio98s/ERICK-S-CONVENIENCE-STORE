2be6f40a633e48bb065c341aba5eb7ad5ce7b8f2
Mauricio Sánchez <sanchezaguilarmauricio56@gmail.com>
2025-11-10 09:41:52 -0500

    testfile changed

--- suma.py
+++ suma.py
@@ -12,13 +12,3 @@ suma = a + b
 print(suma)
 suma = a + b + c + d + e + f + g + h + i + j
 print(suma)
-suma = a + b + c + d + e + f + g + h + i + j + k
-print(suma)
-suma = a + b + c + d + e + f + g + h + i + j + k + l
-print(suma)
-suma = a + b + c + d + e + f + g + h + i + j + k + l + m
-print(suma)
-suma = a + b + c + d + e + f + g + h + i + j + k + l + m + n
-print(suma)
-suma = a + b + c + d + e + f + g + h + i + j + k + l + m + n + o
-print(suma)

5488f0020d43ec0964b424201e79b22880db6a37
Mauricio Sánchez <sanchezaguilarmauricio56@gmail.com>
2025-11-10 09:36:09 -0500

    testfile changed

--- suma.py
+++ suma.py
@@ -1,4 +1,24 @@
 a = 10
 b = 20
+c = 30
+d = 40
+e = 50
+f = 60
+g = 70
+h = 80
+i = 90
+j = 100
 suma = a + b
+print(suma)
+suma = a + b + c + d + e + f + g + h + i + j
+print(suma)
+suma = a + b + c + d + e + f + g + h + i + j + k
+print(suma)
+suma = a + b + c + d + e + f + g + h + i + j + k + l
+print(suma)
+suma = a + b + c + d + e + f + g + h + i + j + k + l + m
+print(suma)
+suma = a + b + c + d + e + f + g + h + i + j + k + l + m + n
+print(suma)
+suma = a + b + c + d + e + f + g + h + i + j + k + l + m + n + o
 print(suma)
